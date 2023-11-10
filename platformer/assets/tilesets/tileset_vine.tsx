109691098d3eb036881bf136868525b3a7e8f594
Aniol Morató <aniol.morato@enti.cat>
2023-11-10 17:23:58 +0100

    Tilesets i nivell 1

---
+++ platformer/assets/tilesets/tileset_vine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_vine" tilewidth="8" tileheight="8" tilecount="2" columns="1">
+ <image source="tileset_vine.png" trans="ff00ff" width="8" height="16"/>
+</tileset>
